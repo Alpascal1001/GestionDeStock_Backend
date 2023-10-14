8963cb35cbaee43e3001eb6533f2f0a92ee0d646
Alpascal <phonse006@gmail.com>
2023-10-14 21:01:52 +0000

    initialisation backend

---
+++ src/main/java/com/gestiondestock/backend/backendgestiondestock/BackendgestiondestockApplication.java
@@ -0,0 +1,13 @@
+package com.gestiondestock.backend.backendgestiondestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendgestiondestockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendgestiondestockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestiondestock/backend/backendgestiondestock/BackendgestiondestockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestiondestock.backend.backendgestiondestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendgestiondestockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
