1dae971a74f631d8b68e6b21a3539e184e2d9ea2
Paterne <diedhioupaterne279@gmail.com>
2023-12-23 17:13:56 +0000

    blocage d'un utilisteur

--- src/main/java/com/gestiondestock/backend/backendgestiondestock/controller/UserController.java
+++ src/main/java/com/gestiondestock/backend/backendgestiondestock/controller/UserController.java
@@ -12,6 +12,7 @@ import com.gestiondestock.backend.enumeration.ETAT_USER;
 
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
 	@Autowired
